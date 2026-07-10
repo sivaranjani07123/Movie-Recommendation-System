2946e4c7f319fe4ad7918a3134eb705a1780b755
sivaranjani07123 <sivaranjani1072006@gmail.com>
2026-07-11 00:35:47 +0530

    Add requirements.txt and deployment files

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
 from flask import Flask, render_template, request, jsonify
 import joblib
+from sklearn.metrics.pairwise import cosine_similarity
 app = Flask(__name__)
 movies = joblib.load("movies.pkl")
-similarity = joblib.load("similarity.pkl")
-@app.route("/") #Home page
+genre_matrix = joblib.load("genre_matrix.pkl")
+@app.route("/")
 def home():
     return render_template("index.html")
-@app.route("/health") #Health check API
+@app.route("/health")
 def health():
     return jsonify({"status": "OK"})
-@app.route("/recommend", methods=["POST"]) #Recommendation API
+@app.route("/recommend", methods=["POST"])
 def recommend():
     try:
         data = request.get_json()
@@ -19,17 +20,23 @@ def recommend():
         if movie_name not in movies["title"].values:
             return jsonify({"error": "Movie not found"}), 404
         movie_index = movies[movies["title"] == movie_name].index[0]
-        similarity_scores = list(enumerate(similarity[movie_index]))
+        scores = cosine_similarity(
+            genre_matrix[movie_index],
+            genre_matrix
+        ).flatten()
+        similarity_scores = list(enumerate(scores))
         similarity_scores = sorted(
             similarity_scores,
             key=lambda x: x[1],
             reverse=True
         )
         recommendations = []
-        for i in similarity_scores[1:6]:
-            recommendations.append(movies.iloc[i[0]]["title"])
+        for movie in similarity_scores[1:6]:
+            recommendations.append(
+                movies.iloc[movie[0]]["title"]
+            )
         return jsonify({"recommendations": recommendations})
     except Exception as e:
         return jsonify({"error": str(e)}), 500
-if __name__ == "__main__": #Running the application
+if __name__ == "__main__":
     app.run(debug=True)
